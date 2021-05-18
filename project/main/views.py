49d706e9b3384b74c71528539c3ae88af3a79ed6
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-05-18 18:38:38 -0400

    communicate with backend

--- project/main/views.py
+++ project/main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 import requests
+import django.core.handlers.wsgi as wsgi
 # Create your views here.
-def index(req):
+def index(req:wsgi.WSGIRequest):
     return render(req, 'index.html')
 
-def byUsername(req):
-    requests.post
+def byUsername(req:wsgi.WSGIRequest):
+    username = req.POST.get('username')
+    request = requests.get('http://127.0.0.1:8000/person/{u}'.format(u=username))
+    print(request.text)
     return render(req, 'index.html')
